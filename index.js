93e5122381c6a1a9e22b522780f92e95e1e5e8a1
Jacob Hoskins <45977627+Jacob-Hoskins@users.noreply.github.com>
2022-03-15 16:35:06 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,154 @@
 
-function changePage(value){
+function changePage(value, bookNumber){
     //button values
     const home = document.getElementById('homePageButton')
     const profile = document.getElementById('profilePageButton')
     const cart = document.getElementById('shoppingCartPageButton')
+    const product = document.getElementById('bookInfo')
 
     //css view values
     const profileCSS = document.getElementsByClassName('profilePage')
     const cartCSS = document.getElementsByClassName('shoppingCart')
     const mainCSS = document.getElementsByTagName('main')
+    const productCSS = document.getElementsByClassName('bookInfo')
+
+    console.log('clicked')
 
     if(value === 'home'){
         mainCSS[0].style.display = 'block'
         cartCSS[0].style.display = 'none'
         profileCSS[0].style.display = 'none'
+        productCSS[0].style.display = 'none'
+
 
     }else if(value ==='profile'){
         mainCSS[0].style.display = 'none'
         cartCSS[0].style.display = 'none'
         profileCSS[0].style.display = 'block'
+        productCSS[0].style.display = 'none'
 
     }else if(value === 'cart'){
         mainCSS[0].style.display = 'none'
         cartCSS[0].style.display = 'block'
         profileCSS[0].style.display = 'none'
+        productCSS[0].style.display = 'none'
+
+    }else if(value === 'product'){
+        mainCSS[0].style.display = 'none'
+        cartCSS[0].style.display = 'none'
+        profileCSS[0].style.display = 'none'
+        productCSS[0].style.display = 'block'
+
+        //element clicked info
+        // const book = document.getElementById('book1')
+        // console.log(book.querySelector('h4'))
+        
+        updateBookPage(bookNumber)
+
     }
 }
+
+//takes value of clicked element, will grab the documents info to fill page info
+function updateBookPage(value){
+    let bookInfo = 'book' + value
+    let bookElement = document.getElementById(bookInfo)
+    let bookTitle = document.getElementById('title')
+    let bookPrice = document.getElementById('price')
+    let bookImg = document.getElementById('image')
+    
+    
+    if(value === 1){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+    
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+        
+    }else if(value === 2){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+    }
+    else if(value === 3){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 4){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 5){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 6){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 7){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 8){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }else if(value === 9){
+        let pageTitle = bookElement.querySelector('h4')
+        let pagePrice = bookElement.querySelector('p')
+        let pageImg = bookElement.querySelector('img')
+        let sourceImg = pageImg.getAttribute('src')
+
+        bookTitle.innerText = pageTitle.textContent
+        bookPrice.innerText = pagePrice.textContent
+        bookImg.src = sourceImg
+
+    }
+
+}
+
+function addToCart(value){}
